b2d3d63356b70bf18033161b1519daeea8892cb7
Bega-jr <85181084+Bega-jr@users.noreply.github.com>
2025-12-19 14:23:05 -0300

    Update results.py

--- app/routes/results.py
+++ app/routes/results.py
@@ -1,8 +1,20 @@
 from fastapi import APIRouter
-from app.services import get_concurso
+from app.services import get_concurso, get_ultimos_concursos
 
-router = APIRouter(prefix="/lotofacil", tags=["Lotofácil"])
+router = APIRouter(
+    prefix="/lotofacil",
+    tags=["Resultados"]
+)
+
 
 @router.get("/concurso/{concurso_id}")
 def concurso(concurso_id: int):
     return get_concurso(concurso_id)
+
+
+@router.get("/ultimos/{quantidade}")
+def ultimos(quantidade: int):
+    return {
+        "quantidade": quantidade,
+        "concursos": get_ultimos_concursos(quantidade)
+    }
