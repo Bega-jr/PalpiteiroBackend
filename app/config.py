cbe038919928f90003282f9a976cf661f13d865c
Bega-jr <85181084+Bega-jr@users.noreply.github.com>
2025-12-19 14:20:35 -0300

    Update config.py

--- app/config.py
+++ app/config.py
@@ -3,7 +3,3 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 DATA_FILE = BASE_DIR / "data" / "Lotofacil.xlsx"
-
-LOTERIA = "Lotofácil"
-MOEDA = "BRL"
-LOCALE = "pt_BR"
