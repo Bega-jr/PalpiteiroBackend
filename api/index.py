a07bbb8f0e4c2c4927bc308d24a04a618fdadf95
Bega-jr <85181084+Bega-jr@users.noreply.github.com>
2025-12-19 17:21:33 -0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from app.routes.health import router as health_router
 from app.routes.estatisticas import router as estatisticas_router
 from app.routes.ultimos import router as ultimos_router
+from app.routes.concurso import router as concurso_router  # 👈 novo
 
 app = FastAPI(
     title="Palpiteiro Backend",
@@ -17,3 +18,4 @@ def root():
 app.include_router(health_router)
 app.include_router(estatisticas_router)
 app.include_router(ultimos_router)
+app.include_router(concurso_router)  # 👈 novo
