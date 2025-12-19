9f96d1748d70d74c4dd554497d69e2fb3bf3bda1
Bega-jr <85181084+Bega-jr@users.noreply.github.com>
2025-12-19 15:51:12 -0300

    Update lotofacil_service.py

--- app/services/lotofacil_service.py
+++ app/services/lotofacil_service.py
@@ -1,6 +1,12 @@
 import pandas as pd
 from app.config import localizar_arquivo_lotofacil
 
-def load_lotofacil_data():
-    path = localizar_arquivo_lotofacil()
-    return str(path)
+
+def load_lotofacil_data() -> pd.DataFrame:
+    """
+    Carrega o arquivo oficial da Lotofácil (XLSX)
+    e retorna um DataFrame pandas.
+    """
+    arquivo = localizar_arquivo_lotofacil()
+    df = pd.read_excel(arquivo, engine="openpyxl")
+    return df
