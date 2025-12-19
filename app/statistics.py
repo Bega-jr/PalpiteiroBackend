f9f14982ce25a3975ce7cd0d9b9605971df46581
Bega-jr <85181084+Bega-jr@users.noreply.github.com>
2025-12-19 15:56:56 -0300

    Update statistics.py

--- app/statistics.py
+++ app/statistics.py
@@ -1,18 +1,27 @@
-from app.services.lotofacil_service import carregar_dataframe
+import pandas as pd
+from app.services.lotofacil_service import load_lotofacil_data
 
 
-def gerar_estatisticas():
-    df = carregar_dataframe()
+def gerar_estatisticas_lotofacil():
+    df = load_lotofacil_data()
 
-    dezenas = []
-    for i in range(1, 16):
-        dezenas.extend(df[f"Bola{i}"].tolist())
+    dezenas = [f"Bola{i}" for i in range(1, 16)]
 
-    frequencia = {i: dezenas.count(i) for i in range(1, 26)}
+    total_concursos = len(df)
 
-    return {
-        "total_concursos": int(df.shape[0]),
-        "frequencia_dezenas": frequencia,
-        "mais_sorteados": sorted(frequencia, key=frequencia.get, reverse=True)[:5],
-        "menos_sorteados": sorted(frequencia, key=frequencia.get)[:5],
+    numeros_frequencia = (
+        df[dezenas]
+        .stack()
+        .value_counts()
+        .sort_index()
+        .to_dict()
+    )
+
+    estatisticas = {
+        "total_concursos": total_concursos,
+        "frequencia_numeros": numeros_frequencia,
+        "numero_mais_sorteado": max(numeros_frequencia, key=numeros_frequencia.get),
+        "numero_menos_sorteado": min(numeros_frequencia, key=numeros_frequencia.get),
     }
+
+    return estatisticas
