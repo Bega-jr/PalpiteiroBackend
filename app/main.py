7083c12dca1b30f5302dbc9a4a245d91ccf2fd2a
Bega-jr <85181084+Bega-jr@users.noreply.github.com>
2025-12-19 16:43:33 -0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
 
-from app.routes.health import router as health_router
-
 app = FastAPI()
 
-app.include_router(health_router)
+
+@app.get("/")
+def root():
+    return {"status": "ROOT_OK"}
