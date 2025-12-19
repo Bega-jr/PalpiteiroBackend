083c6249dff06cd10af29da4831f834346c8cb04
Bega-jr <85181084+Bega-jr@users.noreply.github.com>
2025-12-19 14:52:26 -0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,1 +1,16 @@
-from app.main import app
+from flask import Flask
+from app.routes.lotofacil import lotofacil_bp
+
+def create_app():
+    app = Flask(__name__)
+
+    app.register_blueprint(lotofacil_bp, url_prefix="/lotofacil")
+
+    @app.route("/health")
+    def health():
+        return {"status": "ok"}
+
+    return app
+
+
+app = create_app()
