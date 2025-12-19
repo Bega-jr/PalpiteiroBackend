05176525184323378fd3dbd0050121b6bd49994b
Bega-jr <85181084+Bega-jr@users.noreply.github.com>
2025-12-19 14:21:30 -0300

    Update utils.py

--- app/utils.py
+++ app/utils.py
@@ -1,14 +1,2 @@
-from babel.numbers import format_currency
-from babel.dates import format_date
-from datetime import datetime
-
-def format_brl(valor: float) -> str:
-    return format_currency(valor, "BRL", locale="pt_BR")
-
-def format_data_br(data) -> str:
-    if isinstance(data, str):
-        data = datetime.strptime(data, "%d/%m/%Y")
-    return format_date(data, format="full", locale="pt_BR")
-
 def format_dezena(n: int) -> str:
-    return f"{n:02d}"
+    return f"{int(n):02d}"
