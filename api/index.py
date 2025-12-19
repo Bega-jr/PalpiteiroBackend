a2e8b13d77c58b5ec79471eeee6c193cd1322d6e
Bega-jr <85181084+Bega-jr@users.noreply.github.com>
2025-12-19 15:24:28 -0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,10 +1,13 @@
 from flask import Flask
 
+# teste de import isolado
+from app import config
+
 app = Flask(__name__)
 
 @app.route("/")
 def root():
-    return "OK - Flask subiu"
+    return "OK - Flask + config importado"
 
 @app.route("/health")
 def health():
