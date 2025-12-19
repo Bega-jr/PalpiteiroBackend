1e298bc81a7757708873df3e39faaf6924944041
Bega-jr <85181084+Bega-jr@users.noreply.github.com>
2025-12-19 16:01:11 -0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,13 +1,1 @@
-from flask import Flask
-
-from app.services.lotofacil_service import load_lotofacil_data
-
-app = Flask(__name__)
-
-@app.route("/")
-def root():
-    return "OK - Flask + service importado"
-
-@app.route("/health")
-def health():
-    return {"status": "ok"}
+from app.main import app
