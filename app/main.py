ee7f731127b481b3db34386084b5cd5268577b5c
Bega-jr <85181084+Bega-jr@users.noreply.github.com>
2025-12-19 17:31:24 -0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI
+from app.routes.concurso import router as concurso_router
 
-app = FastAPI()
-
+app = FastAPI(
+    title="Palpiteiro Backend",
+    version="1.0.0"
+)
 
 @app.get("/")
 def root():
     return {"status": "ROOT_OK"}
+
+# 🔥 REGISTRA A ROTA
+app.include_router(concurso_router)
