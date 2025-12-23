50c6a544c618a66812a723e559ab2dfa798f9a6a
Bega-jr <85181084+Bega-jr@users.noreply.github.com>
2025-12-23 16:27:57 -0300

    Update palpites.py

--- app/routes/palpites.py
+++ app/routes/palpites.py
@@ -18,10 +18,10 @@ router = APIRouter(
 @router.get("/fixo")
 def palpite_fixo():
     jogo = gerar_palpite_fixo()
-    return {
-        "tipo": "palpite_fixo",
-        "numeros": jogo
-    }
+    if not jogo:
+        return {"erro": "Estatísticas indisponíveis"}
+    return {"tipo": "palpite_fixo", "numeros": jogo
+           }
 
 
 # =====================================================
