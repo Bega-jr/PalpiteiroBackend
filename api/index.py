2ddab800e3995edc7c38535fa8dcd33126a78656
Bega-jr <85181084+Bega-jr@users.noreply.github.com>
2025-12-23 17:05:03 -0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
 from app.routes.health import router as health_router
-from app.routes.estatisticas import router as estatisticas_router
-from app.routes.ultimos import router as ultimos_router
-from app.routes.concurso import router as concurso_router
-from app.routes.debug import router as debug_router
-from app.routes.palpites import router as palpites_router  # 🔥 AQUI
+#from app.routes.estatisticas import router as estatisticas_router
+#from app.routes.ultimos import router as ultimos_router
+#from app.routes.concurso import router as concurso_router
+#from app.routes.debug import router as debug_router
+#from app.routes.palpites import router as palpites_router  # 🔥 AQUI
  
 app = FastAPI(
     title="Palpiteiro Backend",
@@ -16,8 +16,8 @@ def root():
     return {"status": "ok", "service": "Palpiteiro Backend"}
  
 app.include_router(health_router)
-app.include_router(estatisticas_router)
-app.include_router(ultimos_router)
-app.include_router(concurso_router)
-app.include_router(debug_router)
-app.include_router(palpites_router)  # 🔥 ESSENCIAL
+#app.include_router(estatisticas_router)
+#app.include_router(ultimos_router)
+#app.include_router(concurso_router)
+#app.include_router(debug_router)
+#app.include_router(palpites_router)  # 🔥 ESSENCIAL
