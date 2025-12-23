b3fd3f956920ac3643415261dc0a42113803e85a
Bega-jr <85181084+Bega-jr@users.noreply.github.com>
2025-12-23 17:11:18 -0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -4,7 +4,7 @@ from app.routes.estatisticas import router as estatisticas_router
 from app.routes.ultimos import router as ultimos_router
 from app.routes.concurso import router as concurso_router
 from app.routes.debug import router as debug_router
-#from app.routes.palpites import router as palpites_router  # 🔥 AQUI
+from app.routes.palpites import router as palpites_router  # 🔥 AQUI
  
 app = FastAPI(
     title="Palpiteiro Backend",
@@ -20,4 +20,4 @@ app.include_router(estatisticas_router)
 app.include_router(ultimos_router)
 app.include_router(concurso_router)
 app.include_router(debug_router)
-#app.include_router(palpites_router)  # 🔥 ESSENCIAL
+app.include_router(palpites_router)  # 🔥 ESSENCIAL
