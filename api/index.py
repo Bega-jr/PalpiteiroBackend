07bc6e54a4fec599dd48920d242610d7a7bb88ba
Bega-jr <85181084+Bega-jr@users.noreply.github.com>
2025-12-23 17:09:19 -0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from app.routes.health import router as health_router
 from app.routes.estatisticas import router as estatisticas_router
 from app.routes.ultimos import router as ultimos_router
-#from app.routes.concurso import router as concurso_router
+from app.routes.concurso import router as concurso_router
 #from app.routes.debug import router as debug_router
 #from app.routes.palpites import router as palpites_router  # 🔥 AQUI
  
@@ -18,6 +18,6 @@ def root():
 app.include_router(health_router)
 app.include_router(estatisticas_router)
 app.include_router(ultimos_router)
-#app.include_router(concurso_router)
+app.include_router(concurso_router)
 #app.include_router(debug_router)
 #app.include_router(palpites_router)  # 🔥 ESSENCIAL
