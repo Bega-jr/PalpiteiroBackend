e568de1b5a42ce1b1b4484700a571947841cc233
Bega-jr <85181084+Bega-jr@users.noreply.github.com>
2025-12-23 17:06:40 -0300

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from app.routes.health import router as health_router
-#from app.routes.estatisticas import router as estatisticas_router
+from app.routes.estatisticas import router as estatisticas_router
 #from app.routes.ultimos import router as ultimos_router
 #from app.routes.concurso import router as concurso_router
 #from app.routes.debug import router as debug_router
@@ -16,7 +16,7 @@ def root():
     return {"status": "ok", "service": "Palpiteiro Backend"}
  
 app.include_router(health_router)
-#app.include_router(estatisticas_router)
+app.include_router(estatisticas_router)
 #app.include_router(ultimos_router)
 #app.include_router(concurso_router)
 #app.include_router(debug_router)
