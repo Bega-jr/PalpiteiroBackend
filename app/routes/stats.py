f30279c3fb73da1e223b43eefaa3d9de6bb2b2a1
Bega-jr <85181084+Bega-jr@users.noreply.github.com>
2025-12-29 14:26:22 -0300

    Update stats.py

--- app/routes/stats.py
+++ app/routes/stats.py
@@ -1,18 +1,18 @@
-from fastapi import APIRouter, HTTPException
-from app.statistics import gerar_estatisticas_lotofacil
+# from fastapi import APIRouter, HTTPException
+# from app.statistics import gerar_estatisticas_lotofacil
 
-router = APIRouter(
-    prefix="/lotofacil",
-    tags=["Lotofácil - Estatísticas"]
-)
+# router = APIRouter(
+#     prefix="/lotofacil",
+#     tags=["Lotofácil - Estatísticas"]
+# )
 
 
-@router.get("/estatisticas")
-def estatisticas_lotofacil():
-    try:
-        return gerar_estatisticas_lotofacil()
-    except Exception as e:
-        raise HTTPException(
-            status_code=500,
-            detail=f"Erro ao gerar estatísticas da Lotofácil: {str(e)}"
-        )
+# @router.get("/estatisticas")
+# def estatisticas_lotofacil():
+#     try:
+#         return gerar_estatisticas_lotofacil()
+#     except Exception as e:
+#         raise HTTPException(
+#             status_code=500,
+#             detail=f"Erro ao gerar estatísticas da Lotofácil: {str(e)}"
+#         )
