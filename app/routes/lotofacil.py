855922adf1da3e8ff4eb9b128dce5e4b5a2af874
Bega-jr <85181084+Bega-jr@users.noreply.github.com>
2025-12-29 14:25:51 -0300

    Update lotofacil.py

--- app/routes/lotofacil.py
+++ app/routes/lotofacil.py
@@ -1,26 +1,27 @@
-from fastapi import APIRouter
-from app.services.lotofacil_service import (
-    ultimos_concursos,
-    concurso_por_numero,
-)
-from app.statistics import gerar_estatisticas
+# from fastapi import APIRouter
+# from app.services.lotofacil_service import (
+#     ultimos_concursos,
+#     concurso_por_numero,
+# )
+# from app.statistics import gerar_estatisticas
 
-router = APIRouter(
-    prefix="/lotofacil",
-    tags=["Lotofácil"]
-)
-
-
-@router.get("/ultimos/{quantidade}")
-def ultimos(quantidade: int):
-    return ultimos_concursos(quantidade)
+# router = APIRouter(
+#     prefix="/lotofacil",
+#     tags=["Lotofácil"]
+# )
 
 
-@router.get("/concurso/{numero}")
-def concurso(numero: int):
-    return concurso_por_numero(numero)
+# @router.get("/ultimos/{quantidade}")
+# def ultimos(quantidade: int):
+#     return ultimos_concursos(quantidade)
 
 
-@router.get("/estatisticas")
-def estatisticas():
-    return gerar_estatisticas()
+# @router.get("/concurso/{numero}")
+# def concurso(numero: int):
+#     return concurso_por_numero(numero)
+
+
+# @router.get("/estatisticas")
+# def estatisticas():
+#     return gerar_estatisticas()
+    
