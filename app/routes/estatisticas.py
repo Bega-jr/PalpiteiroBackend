af09d103bb7d3ca3e178e1ddbe183d3a6f079ebd
Bega-jr <85181084+Bega-jr@users.noreply.github.com>
2025-12-31 19:58:42 -0300

    Update estatisticas.py

--- app/routes/estatisticas.py
+++ app/routes/estatisticas.py
@@ -1,27 +1,20 @@
 from fastapi import APIRouter, HTTPException
-from app.services.estatisticas_service import (
-    obter_estatisticas_base,
-    obter_estatisticas_com_score
+from app.repositories.estatisticas_repo import (
+    carregar_estatisticas_base,
+    carregar_estatisticas_score
 )
 
-router = APIRouter(
-    prefix="/estatisticas",
-    tags=["Estatísticas"]
-)
-
+router = APIRouter(prefix="/estatisticas", tags=["Estatísticas"])
 
 @router.get("/base")
 def estatisticas_base():
     try:
         return {
             "status": "ok",
-            "dados": obter_estatisticas_base().to_dict(orient="records")
+            "dados": carregar_estatisticas_base()
         }
     except Exception as e:
-        raise HTTPException(
-            status_code=500,
-            detail=str(e)
-        )
+        raise HTTPException(status_code=500, detail=str(e))
 
 
 @router.get("/score")
@@ -29,10 +22,7 @@ def estatisticas_score():
     try:
         return {
             "status": "ok",
-            "dados": obter_estatisticas_com_score().to_dict(orient="records")
+            "dados": carregar_estatisticas_score()
         }
     except Exception as e:
-        raise HTTPException(
-            status_code=500,
-            detail=str(e)
-        )
+        raise HTTPException(status_code=500, detail=str(e))
