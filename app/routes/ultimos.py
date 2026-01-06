ac21848466fadb7ba192ac873365db7d4850c480
Bega-jr <85181084+Bega-jr@users.noreply.github.com>
2026-01-06 08:57:47 -0300

    Update ultimos.py

--- app/routes/ultimos.py
+++ app/routes/ultimos.py
@@ -5,6 +5,5 @@ router = APIRouter(prefix="/ultimos", tags=["Histórico"])
 
 @router.get("/{quantidade}")
 def listar_ultimos(quantidade: int):
-    """Rota para listagens rápidas e estatísticas usando o CSV local"""
+    """Retorna o histórico do CSV de forma rápida"""
     return carregar_historico_csv(quantidade)
-))
