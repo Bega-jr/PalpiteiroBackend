44e59a9c5d4606b18acee3f5529948ffbba87177
Bega-jr <85181084+Bega-jr@users.noreply.github.com>
2026-01-07 14:14:40 -0300

    Update palpites.py

--- app/routes/palpites.py
+++ app/routes/palpites.py
@@ -1,19 +1,15 @@
 from fastapi import APIRouter
-from app.services.palpites_service import obter_todos_palpites
+from app.services.palpites_service import obter_todos_palpites_debug
 
 router = APIRouter(prefix="/palpites", tags=["Palpites"])
 
 
 @router.get("/debug")
 def debug_palpites():
-    """
-    Endpoint de DEBUG:
-    Retorna os dados crus exatamente como estão no banco.
-    """
-    dados = obter_todos_palpites()
+    dados = obter_todos_palpites_debug()
 
     return {
         "status": "ok",
         "total": len(dados),
-        "dados": dados,
+        "dados": dados
     }
