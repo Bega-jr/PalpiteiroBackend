c867b8e022ba910b2cf87f10f304815fdca2ba36
Bega-jr <85181084+Bega-jr@users.noreply.github.com>
2026-01-07 17:54:41 -0300

    Update home.py

--- app/routes/home.py
+++ app/routes/home.py
@@ -1,18 +1,27 @@
-# app/routes/home.py
 from fastapi import APIRouter, HTTPException
 from app.services.home_service import obter_dados_home
 
 router = APIRouter(prefix="/home", tags=["Home"])
 
 
-@router.get("/ultimo")
-def home_ultimo():
-    dados = obter_dados_home()
+@router.get("")
+def home():
+    try:
+        dados = obter_dados_home()
 
-    if not dados:
+        if not dados:
+            raise HTTPException(
+                status_code=404,
+                detail="Nenhum dado encontrado para a Home"
+            )
+
+        return {
+            "status": "ok",
+            "data": dados
+        }
+
+    except Exception as e:
         raise HTTPException(
-            status_code=404,
-            detail="Dados do último concurso não encontrados"
+            status_code=500,
+            detail=str(e)
         )
-
-    return dados
